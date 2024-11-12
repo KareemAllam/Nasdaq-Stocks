26ca618008777d22e220600f32e1b2176a4fe266
Kareem Allam <Kareem.i.Allam@gmail.com>
2024-11-12 19:27:21 +0200

    adding-test-config

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,13 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: [
+    'module:metro-react-native-babel-preset',
+    '@babel/preset-env',
+    '@babel/preset-typescript',
+  ],
   plugins: [
+    ['@babel/plugin-transform-class-properties', { loose: true }],
+    ['@babel/plugin-transform-private-methods', { loose: true }],
+    ['@babel/plugin-transform-private-property-in-object', { loose: true }],
     [
       'module:react-native-dotenv',
       {
